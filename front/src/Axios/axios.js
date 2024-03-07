bbc2c6a49efc99923928a1e841c9af5bb7755315
Sadhos <nknthakur@gmail.com>
2024-03-07 12:01:15 +0530

    newchange

--- front/src/Axios/axios.js
+++ front/src/Axios/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 const instance = axios.create({
-    baseURL:"http://localhost:8000"
+    // baseURL:"http://localhost:8000"
+    baseURL: "https://todo-app-server-ecru-ten.vercel.app"
 })
 export default instance
